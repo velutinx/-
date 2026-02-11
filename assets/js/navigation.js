c033513672c71b6e6e59ae50af919b3c47630e13
velutinx <velutinx@yahoo.com>
2026-02-11 05:44:15 -0700

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,4 +1,3 @@
-// assets/js/navigation.js
 fetch('/assets/includes/navigation.html')
   .then(response => response.text())
   .then(html => {
@@ -8,19 +7,9 @@ fetch('/assets/includes/navigation.html')
   .catch(err => console.error('Failed to load navigation:', err));
 
 function setupNavLinks() {
-  document.getElementById('navHome').onclick = () => {
-    location.href = "https://velutinx.github.io/";  // or simply "/"
-  };
-  document.getElementById('navCom').onclick = () => {
-    location.href = "/commission.html";
-  };
-  document.getElementById('navArt').onclick = () => {
-    location.href = "/artwork.html";
-  };
-  document.getElementById('navContact').onclick = () => {
-    location.href = "/contact.html";
-  };
-  document.getElementById('navPoll').onclick = () => {
-    location.href = "/poll-website/";
-  };
+  document.getElementById('navHome').onclick = () => location.href = "/";
+  document.getElementById('navCom').onclick = () => location.href = "/commission.html";
+  document.getElementById('navArt').onclick = () => location.href = "/artwork.html";
+  document.getElementById('navContact').onclick = () => location.href = "/contact.html";
+  document.getElementById('navPoll').onclick = () => location.href = "/poll-website/";
 }
